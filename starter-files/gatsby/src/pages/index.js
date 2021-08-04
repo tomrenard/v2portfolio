a16dba1f4b546ed40fc552ec847d90264b0ad4c6
Tom Renard <tom.renard@zoho.com>
2021-08-04 15:18:52 +0200

    initial graphql set up

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,9 +1,23 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function index() {
+export default function index({ data }) {
+  const { projects } = data;
   return (
     <div>
-      <p>Hello Portfolio</p>
+      <p>Hello Portfolio {projects.edges[0].node.name}</p>
     </div>
   );
 }
+
+export const query = graphql`
+  query ProjectQuery {
+    projects: allSanityProject {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`;
---
+++ starter-files/sanity/schemas/project.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'project',
+  title: 'Projects',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Project Name',
+      type: 'string',
+      description: 'Name of the project',
+    },
+  ],
+};
