92a7256665828337cbfd6df02ad7d9b42638bb2f
Tom Renard <tom.renard@zoho.com>
2021-08-17 18:03:45 +0200

    styling projects component

--- starter-files/gatsby/src/pages/about.js
+++ starter-files/gatsby/src/pages/about.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Cursor from '../components/Cursor';
 
 export default function about() {
   return (
     <>
-      <Cursor />
       <div>
         <p>Am I the about page?</p>
       </div>
--- starter-files/gatsby/src/pages/contact.js
+++ starter-files/gatsby/src/pages/contact.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Cursor from '../components/Cursor';
 
 export default function contact() {
   return (
     <>
-      <Cursor />
       <p>I am the contact page</p>
     </>
   );
