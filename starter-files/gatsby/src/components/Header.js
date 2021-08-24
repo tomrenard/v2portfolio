05ec3392d1cd51bb75b8a99a2d4b9ffad095c696
Tom Renard <tom.renard@zoho.com>
2021-08-24 18:46:05 +0200

    correcting margin header

--- starter-files/gatsby/src/components/Header.js
+++ starter-files/gatsby/src/components/Header.js
@@ -6,10 +6,9 @@ import NavFooter from './NavFooter';
 // import Img from 'gatsby-image';
 
 const SectionHeaderStyles = styled.header`
-  height: 96vh;
+  height: 100vh;
   .header-container {
     width: 100%;
-    margin-top: 2rem;
     height: 100%;
     display: flex;
     flex-direction: column;
